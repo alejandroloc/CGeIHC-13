860c241348a4d2e78877e9d0ab7c3cf92b5e95e9
alejandroloc <161877646+alejandroloc@users.noreply.github.com>
2026-05-05 07:57:17 -0600

    Add files via upload

---
+++ Window.cpp
@@ -0,0 +1,231 @@
+#include "Window.h"
+
+Window::Window()
+{
+	
+	
+	fuegoEncendido = false;
+	teclaF_Presionada = false;
+
+	cambioLuces = false;
+	teclaM_Presionada = false;
+	Encenderlamp = true;
+	teclaX = false;
+	faro = 0;
+	teclaC = false;
+
+	width = 800;
+	height = 600;
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+	fuegoEncendido = false;
+	teclaF_Presionada = false;
+	
+	cambioLuces = false;
+	teclaM_Presionada = false;
+	width = windowWidth;
+	height = windowHeight;
+	muevex = 2.0f;
+	avanzaHeli = 0.0f;
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+int Window::Initialise()
+{
+	//Inicialización de GLFW
+	if (!glfwInit())
+	{
+		printf("Falló inicializar GLFW");
+		glfwTerminate();
+		return 1;
+	}
+	//Asignando variables de GLFW y propiedades de ventana
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//para solo usar el core profile de OpenGL y no tener retrocompatibilidad
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	//CREAR VENTANA
+	mainWindow = glfwCreateWindow(width, height, "PracticaXX:Nombre de la practica", NULL, NULL);
+
+	if (!mainWindow)
+	{
+		printf("Fallo en crearse la ventana con GLFW");
+		glfwTerminate();
+		return 1;
+	}
+	//Obtener tamaño de Buffer
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	//asignar el contexto
+	glfwMakeContextCurrent(mainWindow);
+
+	//MANEJAR TECLADO y MOUSE
+	createCallbacks();
+
+
+	//permitir nuevas extensiones
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Falló inicialización de GLEW");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST); //HABILITAR BUFFER DE PROFUNDIDAD
+							 // Asignar valores de la ventana y coordenadas
+							 
+							 //Asignar Viewport
+	glViewport(0, 0, bufferWidth, bufferHeight);
+	//Callback para detectar que se está usando la ventana
+	glfwSetWindowUserPointer(mainWindow, this);
+}
+
+void Window::createCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, ManejaTeclado);
+	glfwSetCursorPosCallback(mainWindow, ManejaMouse);
+}
+GLfloat Window::getXChange()
+{
+	GLfloat theChange = xChange;
+	xChange = 0.0f;
+	return theChange;
+}
+
+GLfloat Window::getYChange()
+{
+	GLfloat theChange = yChange;
+	yChange = 0.0f;
+	return theChange;
+}
+
+
+
+
+void Window::ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+	if (key == GLFW_KEY_Y)
+	{
+		theWindow-> muevex += 1.0;
+	}
+	if (key == GLFW_KEY_U)
+	{
+		theWindow-> muevex -= 1.0;
+	}
+	if (key == GLFW_KEY_I)
+	{
+		theWindow->avanzaHeli -= 0.5f;
+		theWindow->luzHeli = true;
+
+	}
+	if (key == GLFW_KEY_K)
+	{
+		theWindow->avanzaHeli += 0.5f; 
+		theWindow->luzHeli = false;
+	}
+	if (key == GLFW_KEY_M) {
+		if (action == GLFW_PRESS && !theWindow->teclaM_Presionada) {
+			theWindow->cambioLuces = !theWindow->cambioLuces; // Invierte el estado
+			theWindow->teclaM_Presionada = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			theWindow->teclaM_Presionada = false;
+		}
+	}
+	if (key == GLFW_KEY_X) {
+
+		if (action == GLFW_PRESS && !theWindow->teclaX) {
+			theWindow->Encenderlamp = !theWindow->Encenderlamp; 
+			theWindow->teclaX = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			theWindow-> teclaX = false;
+		}
+
+
+
+	}
+	if (key == GLFW_KEY_C) {
+		if (action == GLFW_PRESS && !theWindow->teclaC) {
+			theWindow->faro++; // Siguiente color
+			if (theWindow->faro > 3) {
+				theWindow->faro = 0; 
+			}
+			theWindow->teclaC = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			theWindow->teclaC= false;
+		}
+	}
+	//fuego 
+	if (key == GLFW_KEY_F) {
+		if (action == GLFW_PRESS && !theWindow->teclaF_Presionada) {
+			theWindow->fuegoEncendido = !theWindow->fuegoEncendido; // Invierte el estado
+			theWindow->teclaF_Presionada = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			theWindow->teclaF_Presionada = false;
+		}
+	}
+
+
+
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			theWindow->keys[key] = true;
+			//printf("se presiono la tecla %d'\n", key);
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			theWindow->keys[key] = false;
+			//printf("se solto la tecla %d'\n", key);
+		}
+	}
+}
+
+void Window::ManejaMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (theWindow->mouseFirstMoved)
+	{
+		theWindow->lastX = xPos;
+		theWindow->lastY = yPos;
+		theWindow->mouseFirstMoved = false;
+	}
+
+	theWindow->xChange = xPos - theWindow->lastX;
+	theWindow->yChange = theWindow->lastY - yPos;
+
+	theWindow->lastX = xPos;
+	theWindow->lastY = yPos;
+}
+
+
+Window::~Window()
+{
+	glfwDestroyWindow(mainWindow);
+	glfwTerminate();
+
+}
